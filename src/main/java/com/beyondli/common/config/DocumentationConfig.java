15629fba165e9d8e0bb374e1ec97265bab58ffaf
beyondLi71 <liboyang71@163.com>
2019-06-11 13:16:00 +0800

    添加zuul整合swagger

---
+++ src/main/java/com/beyondli/common/config/DocumentationConfig.java
@@ -0,0 +1,34 @@
+package com.beyondli.common.config;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by beyondLi
+ * Date 2019/6/11 10:25
+ * Desc .
+ */
+@Component
+@Primary
+public class DocumentationConfig implements SwaggerResourcesProvider {
+    @Override
+    public List<SwaggerResource> get() {
+        List resources = new ArrayList<>();
+        resources.add(swaggerResource("shop接口", "/gwshop/v2/api-docs", "1.0"));
+        resources.add(swaggerResource("car接口", "/gwcar/v2/api-docs", "1.0"));
+        return resources;
+    }
+
+    private SwaggerResource swaggerResource(String name, String location, String version) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion(version);
+        return swaggerResource;
+    }
+}
